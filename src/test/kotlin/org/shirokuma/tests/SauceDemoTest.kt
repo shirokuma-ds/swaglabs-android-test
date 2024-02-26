85cb8e9c36a146e66bc149619bb9c48ee143fb1d
shirokuma-ds <59801616+shirokuma-ds@users.noreply.github.com>
2024-02-27 01:19:26 +0900

    Add driver quit

--- src/test/kotlin/org/shirokuma/tests/SauceDemoTest.kt
+++ src/test/kotlin/org/shirokuma/tests/SauceDemoTest.kt
@@ -6,6 +6,7 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.shirokuma.drivers.DriverManager
 import org.shirokuma.testData.products.ItemDetail
 import org.shirokuma.users.User
+import org.testng.annotations.AfterMethod
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
 
@@ -17,9 +18,16 @@ class SauceDemoTest {
     @BeforeMethod(alwaysRun = true)
     fun setup() {
         user = User()
-        //
+        // Check whether the target app have been installed
         assertThat("App need to be installed", user.driver!!.isAppInstalled(DriverManager.appPackage))
+        // Make sure that the app start from the login screen
         user.commonSteps.goToLoginScreen()
+    }
+
+    @AfterMethod(alwaysRun = true)
+    fun cleanUp() {
+        // Quit driver
+        user.driver?.quit()
     }
 
     @Test(priority = 1)
