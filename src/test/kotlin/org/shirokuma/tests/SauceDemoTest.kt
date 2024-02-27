181d64760d4e502e2698671b7a4b283d9de2129d
shirokuma-ds <59801616+shirokuma-ds@users.noreply.github.com>
2024-02-28 00:00:21 +0900

    Add check network condition

--- src/test/kotlin/org/shirokuma/tests/SauceDemoTest.kt
+++ src/test/kotlin/org/shirokuma/tests/SauceDemoTest.kt
@@ -1,8 +1,12 @@
 package org.shirokuma.tests
 
+import io.appium.java_client.android.connection.ConnectionState
 import io.qameta.allure.Feature
 import io.qameta.allure.Story
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.notNullValue
+import org.openqa.selenium.support.ui.WebDriverWait
 import org.shirokuma.drivers.DriverManager
 import org.shirokuma.testData.products.ItemDetail
 import org.shirokuma.users.User
@@ -10,6 +14,7 @@ import org.testng.annotations.AfterMethod
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
+import java.time.Duration
 
 @Feature("Sauce Demo Tests")
 class SauceDemoTest {
@@ -19,8 +24,23 @@ class SauceDemoTest {
     @BeforeMethod(alwaysRun = true)
     fun setup() {
         user = User()
+        // Check whether Internet is enabled, if not then turn on wifi
+        assertThat(user.driver, `is`(notNullValue()))
+        assertThat(user.driver?.connection, `is`(notNullValue()))
+        user.driver?.connection?.run {
+            if (!isWiFiEnabled && !isDataEnabled) {
+                user.driver?.connection = ConnectionState(ConnectionState.WIFI_MASK)
+                val wait = WebDriverWait(user.driver, Duration.ofSeconds(20))
+                wait.until {
+                    user.driver?.connection?.isWiFiEnabled
+                }
+            }
+        }
+
         // Check whether the target app have been installed
         assertThat("App need to be installed", user.driver!!.isAppInstalled(DriverManager.appPackage))
+        // Execute active app to always ensure app is activated in the screen
+        user.driver?.activateApp(DriverManager.appPackage)
         // Make sure that the app start from the login screen
         user.commonSteps.goToLoginScreen()
     }
